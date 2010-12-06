150eedb383cc62b4132da1434e4ac4e339286797
Rich Jones <rich@anomos.info>
2010-12-06 18:29:58 -0500

    cheat at upload

--- src/org/ale/openwatch/uService.java
+++ src/org/ale/openwatch/uService.java
@@ -4,7 +4,10 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.Notification;
@@ -79,79 +82,99 @@ public class uService extends Service{
         System.out.println("File path is..");
         System.out.println(pathToOurFile);
         
-        try
-        {
-            FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile) );
-        
-            URL url = new URL(urlServer);
-            connection = (HttpURLConnection) url.openConnection();
-        
-            // Allow Inputs & Outputs
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setUseCaches(false);
-        
-            // Enable POST method
-            connection.setRequestMethod("POST");
-        
-            connection.setRequestProperty("Connection", "Keep-Alive");
-            connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
-        
-            outputStream = new DataOutputStream( connection.getOutputStream() );
+//        try
+//        {
             
-            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            outputStream.writeBytes("Content-Disposition: form-data; name=\"public_description\"" + lineEnd);
-            outputStream.writeBytes(pubDesc);
-            outputStream.writeBytes(lineEnd);
-            
-            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            outputStream.writeBytes("Content-Disposition: form-data; name=\"private_description\"" + lineEnd);
-            outputStream.writeBytes(privDesc);
-            outputStream.writeBytes(lineEnd);
-            
-            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            outputStream.writeBytes("Content-Disposition: form-data; name=\"location\"" + lineEnd);
-            outputStream.writeBytes(location);
-            outputStream.writeBytes(lineEnd);
-            
-            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            outputStream.writeBytes("Content-Disposition: form-data; name=\"rec_file\"; filename=\"" + pathToOurFile +"\"" + lineEnd);
-            outputStream.writeBytes(lineEnd);
-        
-            bytesAvailable = fileInputStream.available();
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
-        
-            // Read file
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-        
-            while (bytesRead > 0)
-            {
-                outputStream.write(buffer, 0, bufferSize);
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            try {
+                InputStream serverInput = ClientHttpRequest.post(
+                        new java.net.URL(urlServer), 
+                        new Object[] {
+                                      "name", title,
+                                      "public_description", pubDesc,
+                                      "private_description", privDesc,
+                                      "location", location,
+                                      "rec_file", new File(pathToOurFile)
+                                     });
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-        
-            outputStream.writeBytes(lineEnd);
-            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
             
-        
-            // Responses from the server (code and message)
-            int serverResponseCode = connection.getResponseCode();
-            String serverResponseMessage = connection.getResponseMessage();
-            
-            System.out.println(serverResponseCode);
-            System.out.println(serverResponseMessage);
-        
-            fileInputStream.close();
-            outputStream.flush();
-            outputStream.close();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
+//            FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile) );
+//        
+//            URL url = new URL(urlServer);
+//            connection = (HttpURLConnection) url.openConnection();
+//        
+//            // Allow Inputs & Outputs
+//            connection.setDoInput(true);
+//            connection.setDoOutput(true);
+//            connection.setUseCaches(false);
+//        
+//            // Enable POST method
+//            connection.setRequestMethod("POST");
+//        
+//            connection.setRequestProperty("Connection", "Keep-Alive");
+//            connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+//        
+//            outputStream = new DataOutputStream( connection.getOutputStream() );
+//            
+//            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+//            outputStream.writeBytes("Content-Disposition: form-data; name=\"public_description\"" + lineEnd);
+//            outputStream.writeBytes(pubDesc);
+//            outputStream.writeBytes(lineEnd);
+//            
+//            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+//            outputStream.writeBytes("Content-Disposition: form-data; name=\"private_description\"" + lineEnd);
+//            outputStream.writeBytes(privDesc);
+//            outputStream.writeBytes(lineEnd);
+//            
+//            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+//            outputStream.writeBytes("Content-Disposition: form-data; name=\"location\"" + lineEnd);
+//            outputStream.writeBytes(location);
+//            outputStream.writeBytes(lineEnd);
+//            
+//            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+//            outputStream.writeBytes("Content-Disposition: form-data; name=\"rec_file\"; filename=\"" + pathToOurFile +"\"" + lineEnd);
+//            outputStream.writeBytes(lineEnd);
+//        
+//            bytesAvailable = fileInputStream.available();
+//            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+//            buffer = new byte[bufferSize];
+//        
+//            // Read file
+//            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+//        
+//            while (bytesRead > 0)
+//            {
+//                outputStream.write(buffer, 0, bufferSize);
+//                bytesAvailable = fileInputStream.available();
+//                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+//                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+//            }
+//        
+//            outputStream.writeBytes(lineEnd);
+//            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+//            
+//        
+//            // Responses from the server (code and message)
+//            int serverResponseCode = connection.getResponseCode();
+//            String serverResponseMessage = connection.getResponseMessage();
+//            
+//            System.out.println(serverResponseCode);
+//            System.out.println(serverResponseMessage);
+//        
+//            fileInputStream.close();
+//            outputStream.flush();
+//            outputStream.close();
+//        }
+//        catch (Exception ex)
+//        {
+//            ex.printStackTrace();
+//        }
         
         
         System.out.println("Cancelling notification.");
@@ -174,7 +197,7 @@ public class uService extends Service{
         pubDesc = prefs.getString("pub_desc", "No description available");
         privDesc = prefs.getString("priv_desc", "No description available");
         title = prefs.getString("title", "No title available");
-        location = prefs.getString("title", "No location available");
+        location = prefs.getString("location", "No location available");
     }
 
     @Override
