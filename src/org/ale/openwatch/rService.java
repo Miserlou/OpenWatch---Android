3f33dfb5208afd519f5061cce22e9f3469a8b224
hbringo <hbringo@mailinator.com>
2011-11-09 11:02:17 -0800

    Removed some imports which weren't being used. Should lower memory footprint a little and remove some warnings from eclipse.

--- src/org/ale/openwatch/rService.java
+++ src/org/ale/openwatch/rService.java
@@ -1,9 +1,9 @@
 package org.ale.openwatch;
 
-import java.io.File;
-import java.io.FileOutputStream;
+//import java.io.File;
+//import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+//import java.io.OutputStreamWriter;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -11,11 +11,12 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Environment;
+//removed as this isn't used. Re-enable if needed.
+//import android.content.SharedPreferences;
+//import android.os.Environment;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.preference.PreferenceManager;
+//import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.lang.System;
